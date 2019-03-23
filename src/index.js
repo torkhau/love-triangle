20f3c169848575352532c77368f135b408928cf9
Torkhau <torkhau@gmail.com>
2019-03-23 13:09:59 +0300

    refactor: Rewrite come code. Remove global variables. Rewrite function to arrow function.

--- src/index.js
+++ src/index.js
@@ -2,58 +2,45 @@
  * @param preferences - an array of integers. Indices of people, whom they love
  * @returns number of love triangles
  */
-var len;
-
 module.exports = function getLoveTrianglesCount(preferences = []) {
+  const SpichoneIsPass = (spichonee) => {
+    if ((spichonee > 0) || (spichonee <= maxLenForLooking)) {
+      return true
+    };
+    return false;
+  }
 
-  len = getMaxArray(preferences);
+  let maxLenForLooking = preferences[0];
+  for (let i = 1; i < preferences.length; i++) {
+    if (maxLenForLooking < preferences[i]) {
+      maxLenForLooking = preferences[i];
+    }
+  }
   let loveTriangles = 0;
-  let i = 1;
-  for (let i = 1; i <= len; i++) {
+  for (let i = 1; i <= maxLenForLooking; i++) {
     let spichonee1 = preferences[i - 1];
-    if (!spichoneIsPass(spichonee1)) {
+    if (!SpichoneIsPass(spichonee1)) {
       continue
     };
     if (spichonee1 == i) {
       continue;
     } else {
       let spichonee2 = preferences[spichonee1 - 1];
-      if (!spichoneIsPass(spichonee2)) {
+      if (!SpichoneIsPass(spichonee2)) {
         continue
-      };  
+      };
       if (spichonee2 == spichonee1) {
         continue;
       } else {
         let spichonee3 = preferences[spichonee2 - 1];
-        if (!spichoneIsPass(spichonee3)) {
+        if (!SpichoneIsPass(spichonee3)) {
           continue
-        };    
+        };
         if (spichonee3 == i) {
           loveTriangles++;
         };
       };
-    };  
-  };
-  return loveTriangles/3;
-};
-
-function getMaxArray(arr) {
-  
-  const len = arr.length;
-  let max = arr[0];
-  for (let i = 1; i < len; i++) {
-    if (max < arr[i]) {
-      max = arr[i];
     };
   };
-  return max;
-};
-
-function spichoneIsPass(spichonee) {
-
-  let res = false;
-  if ((spichonee > 0) || (spichonee <= len)) {
-    res = true
-  };
-  return res;
+  return loveTriangles / 3;
 };
